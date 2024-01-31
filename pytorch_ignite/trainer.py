895d7d5fddecbebab1fd042927db09a01b56516d
Sadnesstt <dnskdud88@gmail.com>
2024-02-01 06:59:27 +0900

    2024-02-01

---
+++ pytorch_ignite/trainer.py
@@ -0,0 +1,43 @@
+from copy import deepcopy
+
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.nn.utils as torch_utils
+
+from ignite.engine import Engine
+from ignite.engine import Events
+from ignite.metrics import RunningAverage
+from ignite.contrib.handlers.tqdm_logger import ProgressBar
+
+from utils import ge_grad_norm, get_parameter_norm
+
+VERBOSE_SILENT = 0
+VERBOSE_EPOCH_WISE = 1
+VERBOSE_BATCH_WISE = 2
+
+class MyEngine(Engine):
+
+    def __init__(self, func, model, crit, optimizer, config):
+        # Ignite Engine does not have objects in below lines.
+        # Thus, we assign class variables to access these object, during the procedure.
+        self.model = model
+        self.crit - crit
+        self.optimizer = optimizer
+        self.config = config
+
+        super().__init__(func) # Ignite Engine only needs function to run.
+
+        self.best_loss = np.inf
+        self.best_model = None
+
+        self.device = next(model.parameters()).device
+
+    @staticmethod
+    def train(engine, mini_batch):
+        # You have to reset the gradients of all model parameters
+        # before to take another step in gradient descent
+        engine.model.train() # Because we assign model as class variable, we can easily access to it.
+        engine.optimizer.zero_grad()
